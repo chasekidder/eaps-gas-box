6061315b985ee049c57d0b06fc702de42310197e
Chase Kidder <chasekidder@gmail.com>
2020-12-31 13:13:28 -0500

    Modify db to accept sensor info

--- app/forms.py
+++ app/forms.py
@@ -2,6 +2,7 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, IntegerField
 from wtforms.validators import DataRequired
 
+
 class DataCollectionForm(FlaskForm):
     frequency = IntegerField("Frequency (per sec)", validators=[DataRequired()])
     file_name = StringField("File Name", validators=[DataRequired()])
