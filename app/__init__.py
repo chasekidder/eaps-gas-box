a7c924a0d3cf1ac99e3d9258e6e7197f1cfd7f83
Chase Kidder <chasekidder@gmail.com>
2021-01-14 17:18:58 -0500

    fix paths

--- app/__init__.py
+++ app/__init__.py
@@ -3,20 +3,19 @@ from flask_sqlalchemy import SQLAlchemy
 
 __version__ = (1, 0, 0, "dev")
 
-
-
 # Initialize the app
-app = Flask(__name__, 
+frontend = Flask(__name__, 
             instance_relative_config=True,
-            static_folder="static/",
-            static_url_path=""
+            static_folder="ui/static/",
+            static_url_path="",
+            template_folder="ui/templates/"
             )
 
 # Load the views
-from app import views
+from app.ui import views
 
 # Load the config file
-app.config.from_object('config.DevConfiguration')
+frontend.config.from_object('config.DevConfiguration')
 
 # Open the DB Connection
-db = SQLAlchemy(app)
+db = SQLAlchemy(frontend)
