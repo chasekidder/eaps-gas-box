8f6fde97b2550cb9640bd71be9870bf0e8064634
Chase Kidder <chasekidder@gmail.com>
2021-01-24 21:02:06 -0500

    fix register

--- app/sensors/sensor_TEROS12.py
+++ app/sensors/sensor_TEROS12.py
@@ -45,6 +45,6 @@ class TEROS12(Sensor):
     def read_sensor(self) -> str:
         command_string = [ord(c) for c in f"{ self.ADDRESS }R0!"]
         self.bus.write_i2c_block_data(ARDUINO_NANO_I2C_ADDRESS, NANO_I2C_CMD.CMD_REG_WRITE, command_string)
-        value = self.bus.read_i2c_block_data(ARDUINO_NANO_I2C_ADDRESS, NANO_I2C_CMD.A_READ_A0, 16)
+        value = self.bus.read_i2c_block_data(ARDUINO_NANO_I2C_ADDRESS, NANO_I2C_CMD.SDI12_READ, 16)
 
         return value
