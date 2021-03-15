5a0ecae3a8bddf6d93f73e1062b6e1ea77931282
Chase Kidder <chasekidder@gmail.com>
2021-03-15 10:39:24 -0400

    f

--- sensors/sensors.py
+++ sensors/sensors.py
@@ -129,6 +129,7 @@ class LOX02F(Sensor):
             value = self.bus.read_i2c_block_data(NANO_I2C_ADDR, NANO.UART1_READ, 32)
             time.sleep(0.1)
 
+        print("---------------------")
         print(value)
 
 
